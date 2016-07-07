698b83dc4e843b95bfc99e4935bdefcb8df70fcb
zhangsiyuan <1021808625@qq.com>
2016-07-07 14:01:21 +0800

    配置 API_ROOT

---
+++ FE/src/config.js
@@ -0,0 +1,3 @@
+export const API_ROOT = (process.env.NODE_ENV === 'production')
+  ? 'http://jiangjiu.leanapp.cn/'
+  : 'http://localhost:3000/'
--- FE/src/vuex/actions.js
+++ FE/src/vuex/actions.js
@@ -1,8 +1,9 @@
 import * as types from './mutation-types'
+import {API_ROOT} from '../config'
 
 export const getContentList = function ({dispatch}) {
   dispatch(types.REQUEST_CONTENT_LIST)
-  this.$http.get('http://localhost:3000/api/content-list').then(response => {
+  this.$http.get(API_ROOT + 'api/content-list').then(response => {
     dispatch(types.GET_CONTENT_LIST, JSON.parse(response.body))
   }, error => {
     dispatch(types.GET_CONTENT_LIST_FAILURE, error)
